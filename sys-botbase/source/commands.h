b590a2b17b9c0995006a376bafefb6448902d00d
Eppin <96589679+Eppin@users.noreply.github.com>
2023-06-17 20:15:11 +0200

    Test day skip for SV

--- sys-botbase/source/commands.h
+++ sys-botbase/source/commands.h
@@ -76,5 +76,6 @@ void touch(HidTouchState* state, u64 sequentialCount, u64 holdTime, bool hold, u
 void key(HiddbgKeyboardAutoPilotState* states, u64 sequentialCount);
 void clickSequence(char* seq, u8* token);
 void dateSkip();
+void dateSkip2();
 void resetTime();
 void sendUsbResponse(USBResponse response);
